9b20b494e92e95c1ebe3046b4734c754de1a9543
Noam Musba <noammu@wix.com>
2024-07-03 15:07:38 +0300

    ff

--- src/_codux/boards/cgfhfdghfgh/cgfhfdghfgh.board.tsx
+++ src/_codux/boards/cgfhfdghfgh/cgfhfdghfgh.board.tsx
@@ -2,7 +2,12 @@ import { createBoard } from '@wixc3/react-board';
 import { Cgfhfdghfgh } from '../../../components/cgfhfdghfgh/cgfhfdghfgh';
 
 export default createBoard({
-    name: 'Cgfhfdghfgh',
+    name: 'ghjghj',
     Board: () => <Cgfhfdghfgh />,
     isSnippet: true,
+    tags: ['merged'],
+    environmentProps: {
+        windowWidth: 1024,
+        windowHeight: 768,
+    },
 });
